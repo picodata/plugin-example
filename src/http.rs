6e2c28034456d804b4207c02e8b8f9d0bf92c229
d.koltsov <dkoltsov@picodata.io>
2024-07-02 17:33:50 +0300

    feat(cache): implement weather_cache MVP

--- src/http.rs
+++ src/http.rs
@@ -1,17 +1,108 @@
-use std::error::Error;
-use serde::Serialize;
+use std::{error::Error, time::Duration};
 
-#[derive(Clone, Serialize)]
-pub(crate) struct WeatherInfo {
-    city: String,
-    temperature: i32,
+use picoplugin::{sql::types::SqlValue, system::tarantool::tuple::Tuple};
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+struct CurrentWeather {
+    temperature_2m: f64,
 }
 
-pub(crate) fn weather_handler() -> Result<WeatherInfo, Box<dyn Error>> {
-    Ok(WeatherInfo{
-        city: "Empty".into(),
-        temperature: 0,
-    })
+
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+struct StoredWeatherInfo {
+    id: picoplugin::system::tarantool::uuid::Uuid,
+    latitude: f64,
+    longitude: f64,
+    temperature: f64,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+pub(crate) struct WeatherInfo {
+    latitude: f64,
+    longitude: f64,
+    current: CurrentWeather,
+}
+
+static METEO_URL: once_cell::sync::Lazy<String> = once_cell::sync::Lazy::new(|| {
+    std::env::var("METEO_URL").unwrap_or(String::from("https://api.open-meteo.com"))
+});
+
+fn weather_request(latitude: f64, longitude: f64, request_timeout: u64) -> Result<WeatherInfo, Box<dyn Error>> {
+    let http_client  = fibreq::ClientBuilder::new().build();
+    let http_req = http_client
+        .get(format!(
+            "{url}/v1/forecast?\
+            latitude={latitude}&\
+            longitude={longitude}&\
+            current=temperature_2m",
+            url = METEO_URL.as_str(),
+            latitude = latitude,
+            longitude = longitude
+        )).unwrap();
+        
+    let mut http_resp = http_req.request_timeout(Duration::from_secs(request_timeout)).send().unwrap();
+    
+    let resp_body = http_resp.text().unwrap();
+    let info = serde_json::from_str(&resp_body).unwrap();
+
+    return Ok(info)
+}
+
+
+pub(crate) fn weather_handler(latitude: f64, longitude: f64) -> Result<WeatherInfo, Box<dyn Error>> {
+    let select_query: &str = r#"
+    select * from "weather" 
+    where 
+        (latitude < (? + 0.5) AND latitude > (? - 0.5))
+        AND
+        (longitude < (? + 0.5) AND longitude > (? - 0.5));
+    "#;
+    let res = picoplugin::sql::query(&select_query, vec![
+        SqlValue::double(latitude),
+        SqlValue::double(latitude),
+        SqlValue::double(longitude),
+        SqlValue::double(longitude),
+    ]).unwrap();
+
+    let rows_val = res.field::<rmpv::Value>(0).unwrap().unwrap().as_map().unwrap()[1].clone();    
+    let row: Vec<StoredWeatherInfo> = rmpv::ext::from_value(rows_val.1).unwrap();
+
+    let cache_is_empty = row.is_empty();
+    if !cache_is_empty {
+        let weather_info = WeatherInfo{
+            latitude: row[0].latitude,
+            longitude: row[0].longitude,
+            current: CurrentWeather{
+                temperature_2m: row[0].temperature,
+            }
+        };
+        return Ok(weather_info)
+    }
+    let request_timeout = 5;
+
+    let res = match weather_request(latitude, longitude, request_timeout) {
+        Ok(weather_info) => weather_info,
+        Err(e) => panic!("failed to run request %{e}"),
+    };
+
+    let insert_query: &str = r#"
+        INSERT INTO "weather"
+        VALUES(?, ?, ?, ?)
+    "#;
+    let uuid = picoplugin::system::tarantool::uuid::Uuid::random();
+    let _ = picoplugin::sql::query(&insert_query, vec![
+        SqlValue::uuid(uuid),
+        SqlValue::double(res.latitude),
+        SqlValue::double(res.longitude),
+        SqlValue::double(res.current.temperature_2m),
+    ]).unwrap();
+
+    Ok(res)
+}
+
+pub(crate) fn hello_handler() -> Result<String, Box<dyn Error>> {
+    Ok("Hello, world".into())
 }
 
 macro_rules! wrap_http_result {
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use picoplugin::plugin::interface::{CallbackResult, ErrorBox, DDL};
+use std::collections::HashMap;
+
+use picoplugin::plugin::interface::{CallbackResult, DDL};
 use picoplugin::plugin::prelude::*;
 use picoplugin::system::tarantool::tlua;
 use serde::{Deserialize, Serialize};
@@ -40,15 +42,62 @@ impl Service for WeatherService {
     fn on_start(&mut self, ctx: &PicoContext, cfg: Self::CFG) -> CallbackResult<()> {
         let lua = picoplugin::system::tarantool::lua_state();
         lua.exec_with(
-            "pico.httpd:route({method = 'GET', path = 'api/v1/weather' }, ...)",
+            "pico.httpd:route({method = 'GET', path = '/hello' }, ...)",
             tlua::Function::new(|| -> _ {
-                http::wrap_http_result!(http::weather_handler)
+                http::wrap_http_result!(http::hello_handler())
             }),
         ).unwrap();
+        
+        lua.exec_with(
+            "
+            local function make_json_handler(fn)
+                return function(req)
+                    return fn(req.query)
+                end
+            end
+
+            pico.httpd:route(
+                { method = 'GET', path = 'api/v1/weather' },
+                    make_json_handler(...)
+            )
+            ",
+            tlua::function1(move |query: String| -> _ {
+                let params: HashMap<String, f64> = serde_qs::from_str(&query).unwrap();
+                let longitude = params.get("longitude").unwrap();
+                let latitude = params.get("latitude").unwrap();
+                http::wrap_http_result!(http::weather_handler(*longitude, *latitude))
+            }),
+        ).unwrap();
+                
         Ok(())
     }
 
     fn on_stop(&mut self, ctx: &PicoContext) -> CallbackResult<()> {
+        let lua = picoplugin::system::tarantool::lua_state();
+        lua.exec(
+            r#"
+            local httpd = pico.httpd
+            if httpd ~= nil then
+                for n = 1, table.maxn(httpd.routes) do
+                    local r = httpd.routes[n]
+                    if r == nil then
+                        goto continue
+                    end
+                    if not r.name or not r.name:startswith("kirovets-api") then
+                        goto continue
+                    end
+        
+                    log.info("Removing kirovets HTTP route %q (%s)", r.path, r.method)
+                    if httpd.iroutes[r.name] ~= nil then
+                        httpd.iroutes[r.name] = nil
+                    end
+                    httpd.routes[n] = nil
+        
+                    ::continue::
+                end
+            end
+        "#
+        ).unwrap();
         Ok(())
     }
 
@@ -66,5 +115,5 @@ impl WeatherService {
 
 #[service_registrar]
 pub fn service_registrar(reg: &mut ServiceRegistry) {
-    reg.add("ServiceExample", "0.1.0", WeatherService::new);
+    reg.add("weather_service", "0.1.0", WeatherService::new);
 }
